658c1b0f5ee60939164d7db3abbee6d2fc5802d1
Student-03-Bob <70022992+Student-03-Bob@users.noreply.github.com>
2020-08-21 15:04:56 +0100

    Update test01.py

--- test01.py
+++ test01.py
@@ -1,13 +1,16 @@
 import unittest
-from practice01 import  add
+from practice01 import add
+from practice01 import subtract
+from practice01 import multiply
+
 
 class TestDemo(unittest.TestCase):
 
     def test_add(self):
-        assert add(10, 5) == 15
+        self.assertEqual(add(10, 5), 15)
 
     def test_subtract(self):
-        #TODO
+        self.assertEqual(subtract(10, 5), 5)
 
     def test_multiply(self):
-        #TODO
+        self.assertEqual(multiply(10, 5), 50)
